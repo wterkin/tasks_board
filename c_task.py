315d00b399b6cb86ce3dff76c0f7c385f313eda6
wterkin <appmailbox@mail.ru>
2021-03-03 19:12:15 +0300

    В таблицу задач добавлено поле гуида

--- c_task.py
+++ c_task.py
@@ -4,11 +4,16 @@
 from sqlalchemy import Table, Column, String, Text
 
 import c_ancestor as anc
+import uuid
 
 class CTask(anc.CAncestor):
     """Класс справочника событий."""
 
     __tablename__ = 'tbl_tasks'
+    
+    fguid = Column(String,
+                   nullable=False,
+                   unique=True)
     fdescription = Column(String,
                           nullable=False,
                           unique=True)
@@ -18,12 +23,25 @@ class CTask(anc.CAncestor):
     def __init__(self, pdescription, pnotice):
         """Конструктор"""
         super().__init__()
+        self.__GUID_generation()
         self.fdescription = pdescription
         self.fnotice = pnotice
+        
 
+    def __GUID_generation(self):
+        """Генерирует GUID."""
+        guid_string = str(uuid.uuid1())
+        guid_list = guid_string.split("-")
+        self.fguid = "".join(guid_list)
 
+    
     def __repr__(self):
         ancestor_repr = super().__repr__()
         return f"""{ancestor_repr},
                    Desc:{self.fdescription},
                    Notice:{self.fnotice}"""
+
+
+    def get_GUID(self):
+        """Возвращает сгенерированный GUID."""
+        return fguid
