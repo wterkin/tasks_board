93dd3553c1f8bcc617a61ee1bf2bcd7a5355cb06
wterkin <appmailbox@mail.ru>
2021-03-03 13:04:24 +0300

    Функция __save_task в разработке

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 ## -*- coding: utf-8 -*-
 # @author: Andrey Pakhomenkov pakhomenkov@yandex.ru
 """ Органайзер задач. """
+
 import sys
 
 from pathlib import Path
@@ -34,6 +35,8 @@ class CMainWindow(QtWidgets.QMainWindow):
         # *** Интерфейс
         ui_folder = self.application_folder / FORM_FOLDER / MAIN_WINDOW_FORM
         uic.loadUi(self.application_folder / FORM_FOLDER / MAIN_WINDOW_FORM, self)
+        self.setWindowTitle(f"Tasks board ver. {PROGRAM_VERSION} : \"{HEADER_TEXT}\"")
+        self.setWindowIcon(QtGui.QIcon('ui/tasks_board.ico'))
 
         # *** Конфигурация
         self.config = cfg.CConfiguration()
@@ -42,10 +45,6 @@ class CMainWindow(QtWidgets.QMainWindow):
         self.__db_connect()
         if not self.__db_exists():
             self.__db_create()
-        window_title = f"Tasks board ver. {PROGRAM_VERSION} : \"{HEADER_TEXT}\""
-        self.setWindowTitle(window_title)
-        self.setWindowIcon(QtGui.QIcon('ui/tasks_board.ico'))
-        # self.update()
         # *** Компоненты
         # comboBox_Contexts
         # lineEdit_TagsFilter
@@ -65,16 +64,13 @@ class CMainWindow(QtWidgets.QMainWindow):
         self.toolButton_Apply.clicked.connect(self.__save_task)
         self.toolButton_TagsFilter.clicked.connect(self.__set_tags_filter)
         self.toolButton_TextFilter.clicked.connect(self.__set_text_filter)
-        self.pushButton_AddTask.clicked.connect(self.__add_task)
+        self.pushButton_EditTask.clicked.connect(self.__edit_task)
         self.pushButton_DeleteTask.clicked.connect(self.__delete_task)
+
+        # self.update()
         self.show()
 
     
-    def __add_task(self):
-        """Добавляет новую задачу"""
-        pass
-
-
     def __db_connect(self):
         """Устанавливает соединение с БД."""
         self.engine = create_engine('sqlite:///'+self.config.restore_value(cfg.DATABASE_FILE_KEY))
@@ -89,20 +85,44 @@ class CMainWindow(QtWidgets.QMainWindow):
         anc.Base.metadata.create_all()
 
 
+    def __db_disconnect(self):
+        """Разрывает соединение с БД."""
+        self.session.close()
+        self.engine.dispose()
+
+
     def __db_exists(self):
         """Проверяет наличие базы данных по пути в конфигурации."""
         db_folder_path = Path(self.config.restore_value(cfg.DATABASE_FILE_KEY))
         return db_folder_path.exists()
 
-    
+
     def __delete_task(self):
         """Удаляет задачу"""
         pass
 
 
+    def __edit_task(self):
+        """Добавляет новую задачу"""
+        pass
+
+
     def __save_task(self):
         """Сохраняет введённую задачу."""
-        pass
+        # *** Соберём введенные теги
+        entered_tags = self.lineEdit_Tags.text()
+        tag_name_list = entered_tags.split()
+        # *** Поищем введенные теги в базе
+        tag_id_list = []
+        for tag in tag_name_list:
+            
+            tag_id = self.session.query(tag.CTag.id).filter_by(fname=tag).first()
+            if tag_id is not None:
+            
+                tag_id_list.append(tag_id)
+                tag_index = tag_name_list.index(tag)
+                del tag_name_list[tag_index]
+        
 
 
     def __set_tags_filter(self):
