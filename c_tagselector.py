6535a4c395ae4c7a20f4598289cd496a57bd0756
Andrey Pakhomenkov <pakhomenkov.ap@gmail.com>
2022-06-17 09:14:24 +0300

    Селектор тегов заполняется тегами из базы

--- c_tagselector.py
+++ c_tagselector.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # @author: Andrey Pakhomenkov pakhomenkov@yandex.ru
 """Модуль класса селектора тегов."""
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5 import uic
 import c_tag
 from datetime import datetime
@@ -21,22 +21,27 @@ class CTagSelector(QtWidgets.QMainWindow):
         self.application_folder = papplication_folder
 
         uic_path = self.application_folder / "ui" / "tag_selector.ui"
-        print("TS:LTL:path ", uic_path)
+        # print("TS:LTL:path ", uic_path)
         uic.loadUi(uic_path, self)
-
-        tag_list = self.load_tag_list()
-        print("TS:LTL:list ", tag_list)
+        self.scroll_widget = QtWidgets.QWidget()
+        self.scrollArea.setWidget(self.scroll_widget)
+        self.scroll_layout = QtWidgets.QVBoxLayout()
+        self.scroll_widget.setLayout(self.scroll_layout)
+        self.tag_list: list = self.load_tag_list()
+        print("TS:LTL:list ", self.tag_list)
+        self.fill_scrollbox()
         self.show()
 
     def load_tag_list(self):
         """Загружает список тэгов из базы."""
         return self.database.get_session().query(c_tag.CTag.fname).all()
         # query = query.filter(c_tag.CTag.fname.like(f"%{tag_name}%"))
-""" 
-for tag in tag_list[0]:
-    # actions.append(menu.addAction(tag))
-    menu_action = QtWidgets.QAction(tag, self)
-    menu_action.setData(tag)
-    menu_action.triggered.connect(self.actionClicked)
-    menu.addAction(menu_action)
-"""
+
+    def fill_scrollbox(self):
+        """Заполняет скроллбокс чекбоксами"""
+        for tag in self.tag_list:
+
+            checkbox = QtWidgets.QCheckBox(tag[0])
+            checkbox.setCheckState(QtCore.Qt.Unchecked)
+            self.scroll_layout.addWidget(checkbox)
+            print(tag[0])
